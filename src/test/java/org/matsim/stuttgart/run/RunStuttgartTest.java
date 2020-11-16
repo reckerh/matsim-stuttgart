adf954fdf685c0a0fe4665c803f7b69c39fcaefc
david.wedekind <davidj.wedekind@gmail.com>
2020-11-16 23:11:59 +0100

    Switch from la parking cost handler to simple parking cost handler at vsp contrib

--- src/test/java/org/matsim/stuttgart/run/RunStuttgartTest.java
+++ src/test/java/org/matsim/stuttgart/run/RunStuttgartTest.java
@@ -12,7 +12,7 @@ public class RunStuttgartTest {
     @Ignore
     public void testRunnerWithAllExtensions(){
 
-        String configInput = "C:/Users/david/OneDrive/02_Uni/02_Master/05_Masterarbeit/03_MATSim/02_runs/stuttgart-v1.0/stuttgart-v1.0_test/stuttgart-v1.0-0.001pct.config_test.xml" ;
+        String configInput = "C:/Users/david/OneDrive/02_Uni/02_Master/05_Masterarbeit/03_MATSim/02_runs/stuttgart-v1.0/02_stuttgart-v1.0_test/stuttgart-v1.0-0.001pct.config_test.xml" ;
         Config config = RunStuttgartWithAdditionalModules.prepareConfig( new String[]{configInput} ) ;
 
         // Some config modifications for the reduced test
