c2318547f14b39c803e1e68a26bac42dbb022e25
david.wedekind <davidj.wedekind@gmail.com>
2021-01-26 11:06:13 +0100

    Split prepareScenario into two parts - for handling reasons within scenario creation in the masterarbeits repo

--- src/test/java/org/matsim/stuttgart/run/StuttgartMasterThesisRunTest.java
+++ src/test/java/org/matsim/stuttgart/run/StuttgartMasterThesisRunTest.java
@@ -13,15 +13,14 @@ public class StuttgartMasterThesisRunTest {
     public void testRunnerWithAllExtensions(){
 
         String configInput = "C:/Users/david/OneDrive/02_Uni/02_Master/05_Masterarbeit/03_MATSim/02_runs/stuttgart-v1.0/02_stuttgart-v1.0_test/stuttgart-v1.0-0.001pct.config_test.xml" ;
-
         Config config = StuttgartMasterThesisRunner.prepareConfig(new String[]{configInput} ) ;
-
         // Some config modifications for the reduced test
         config.controler().setLastIteration(2) ;
 
-        String fareZoneShapeFileName = "C:/Users/david/OneDrive/02_Uni/02_Master/05_Masterarbeit/03_MATSim/02_runs/stuttgart-v1.0/02_stuttgart-v1.0_test/input/fareZones_sp.shp";
-        String parkingZoneShapeFileName = "C:/Users/david/OneDrive/02_Uni/02_Master/05_Masterarbeit/03_MATSim/02_runs/stuttgart-v1.0/02_stuttgart-v1.0_test/input/parkingShapes.shp";
-        Scenario scenario = StuttgartMasterThesisRunner.prepareScenario(config, fareZoneShapeFileName, parkingZoneShapeFileName) ;
+        String fareZoneShapeFile = "C:/Users/david/OneDrive/02_Uni/02_Master/05_Masterarbeit/03_MATSim/02_runs/stuttgart-v1.0/02_stuttgart-v1.0_test/input/fareZones_sp.shp";
+        String parkingZoneShapeFile = "C:/Users/david/OneDrive/02_Uni/02_Master/05_Masterarbeit/03_MATSim/02_runs/stuttgart-v1.0/02_stuttgart-v1.0_test/input/parkingShapes.shp";
+        Scenario scenario = StuttgartMasterThesisRunner.prepareScenario(config) ;
+        StuttgartMasterThesisRunner.finishScenario(scenario, fareZoneShapeFile, parkingZoneShapeFile);
 
         Controler controler = StuttgartMasterThesisRunner.prepareControler(scenario) ;
         controler.run() ;
